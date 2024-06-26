bdf35b6c10c1f2bb08bb34a9d6ffe2c111a78183
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-06-26 17:27:04 -0300

    Adjust responsivity

--- src/routes/accepted.tsx
+++ src/routes/accepted.tsx
@@ -21,9 +21,9 @@ export function Accepted() {
         <Waves />
         <Balloons />
 
-        {/* <audio ref={audioRef}>
+        <audio ref={audioRef}>
           <source src={romanticMusic} type="audio/mp3" />
-        </audio> */}
+        </audio>
 
         <div className="bg-slate-100 w-screen h-screen flex items-center justify-center flex-col">
           <div className="size-64 z-10 shadow-2xl rounded-full overflow-hidden flex items-center justify-center">
@@ -42,17 +42,13 @@ export function Accepted() {
             <PartyPopper color="#F2575F" size={50} />
           </div>
 
-          {/* Então agora estamos oficialmente saindo 💐. Se arrume bem linda, (não
-        que seja possível você ficar mais linda ainda 😍), pois mais tarde eu
-        estou passando ai pra te buscar 😎. */}
-          <p className="text-slate-700 text-base font-thin mt-6 w-3/4 z-10 max-sm:text-center max-sm:w-11/12">
-            Non ad veniam tempor anim fugiat eu sunt fugiat nulla amet in culpa.
-            Proident nisi ex deserunt irure ut laboris id aliqua excepteur in
-            eiusmod nisi sunt. Ipsum labore nisi culpa aliquip elit enim sint
-            commodo. Tempor et amet irure ipsum sint ad quis exercitation minim.
-            Est nisi aliqua ullamco ipsum fugiat est aliquip aliquip quis. In ea
-            aliquip id pariatur nisi reprehenderit consequat laborum laborum
-            commodo aliquip sint. Est minim magna cupidatat et.
+          <p className="text-slate-700 text-base font-thin mt-6 w-2/4 z-10 max-sm:text-center max-sm:w-11/12">
+            Então agora estamos oficialmente saindo 💐
+          </p>
+
+          <p className="text-slate-700 text-base font-thin w-2/4 z-10 max-sm:text-center max-sm:w-11/12">
+            Se arrume bem linda, (não que seja possível você ficar mais linda
+            ainda 😍), pois mais tarde eu estou passando ai pra te buscar 😎
           </p>
         </div>
       </div>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -7,8 +7,6 @@ import { Waves } from "../components/waves";
 import questionSvg from "../assets/questionSvg.svg";
 
 export function Home() {
-  // https://static.vecteezy.com/system/resources/previews/024/791/093/non_2x/questions-and-answers-find-decision-problem-solving-and-qa-business-decisions-landing-page-isometric-illustration-vector.jpg
-
   const navigate = useNavigate();
 
   const handleRandomMoveNoButton = useCallback(() => {
@@ -33,45 +31,50 @@ export function Home() {
     <div className="overflow-hidden">
       <Waves />
 
-      <div className="bg-slate-100 w-screen h-screen flex items-center justify-between flex-row max-sm:flex-col">
-        <div className="w-2/4 flex flex-col items-center justify-between z-10 pl-36 py-[25%] max-sm:pl-0 max-sm:h-2/4 max-sm:justify-center max-sm:mt-[20%] ">
+      <div className="bg-slate-100 w-screen h-screen flex items-center justify-between flex-row max-sm:flex-col max-sm:pt-4">
+        <div className="w-2/4 flex flex-col items-center justify-between z-10 pl-36 py-[25%] max-sm:pl-0 max-sm:h-3/4 max-sm:justify-center max-sm:mt-[20%]">
           <div className="w-full flex flex-col items-start justify-start">
             <h1 className="text-main-text text-left text-8xl font-bold -ml-2">
               Oi,
             </h1>
-            {/* gostaria de vir em um date comigo? */}
-            <h1 className="text-main-text text-xl font-semibold mt-1 max-sm:mt-0">
-              Ea elit elit est commodo cupidatat voluptate?
+
+            <h1 className="text-main-text text-2xl font-semibold mt-1 max-sm:mt-0">
+              gostaria de vir em um date comigo?
             </h1>
-            {/* Razões para vir em um date comigo u.u */}
+
             <h1 className="text-main-text text-sm font-semibold mt-10">
-              Ea elit elit est commodo cupidatat voluptate? 😌
+              Razões para ir dar uma volta comigo 😌
             </h1>
-            {/* 
-            Caso esteja se perguntando quais as vantagens de sair comigo, são
-              várias 😎. Sou um cara romântico e super educando 🥰, vou lhe
-              tratar igual uma princesa 👑. Vamos ir passear em lugares bonitos,
-              e eu tentarei sempre te fazer rir 😄! Claro que não esquecerei a
-              comida 🍔, vou escolher algo bem especial para gente comer. Também
-              terei o cuidado de ir te buscar em casa e te levar de volta em
-              segurança 🚗.
-            */}
-            <p className="text-slate-600 text-sm font-thin mt-1x">
-              Incididunt fugiat fugiat id non minim velit cupidatat occaecat
-              enim qui commodo pariatur. Voluptate tempor exercitation veniam
-              culpa occaecat laborum sunt dolor aliqua qui eu nulla laborum ea.
-              Veniam non nisi tempor anim ex mollit exercitation sint. Proident
-              elit id non ipsum. In deserunt tempor reprehenderit cillum non
-              cillum ut magna exercitation do proident. Ex do culpa officia sint
-              adipisicing cillum. Nisi occaecat ex esse proident nostrud cillum
-              sint ullamco irure consequat exercitation elit ad.
+
+            <p className="text-slate-600 text-sm font-thin mt-1">
+              Caso esteja se perguntando quais as vantagens de sair comigo, são
+              várias 😎
+            </p>
+
+            <p className="text-slate-600 text-sm font-thin mt-1">
+              Sou um cara super romântico, educando e de boa família 🌹
+            </p>
+
+            <p className="text-slate-600 text-sm font-thin">
+              Vou te levar para passear em lugares legais, e eu tentarei sempre
+              te fazer rir 😄
+            </p>
+
+            <p className="text-slate-600 text-sm font-thin">
+              E claro que não esquecerei a comida 🍔, vou escolher algo bem
+              especial para gente comer
+            </p>
+
+            <p className="text-slate-600 text-sm font-thin">
+              Também terei o cuidado de ir te buscar em casa e te levar de volta
+              em segurança 🚗
             </p>
           </div>
 
-          <div className="flex flex-row w-full items-center justify-start gap-8 mt-32 max-sm:mt-8">
+          <div className="flex flex-row w-full items-center justify-start gap-8 mt-32 max-sm:mt-8 max-sm:flex-col max-sm:gap-2">
             <button
               onClick={handleClickOnYesButton}
-              className="flex flex-row items-center justify-center bg-green-500 w-40 h-10 rounded-lg transition-all text-2xl font-bold text-slate-100 shadow-lg hover:bg-green-600 hover:shadow-xl"
+              className="flex flex-row items-center justify-center bg-green-500 w-40 h-10 rounded-lg transition-all text-2xl font-bold text-slate-100 shadow-lg hover:bg-green-600 hover:shadow-xl max-sm:w-52"
             >
               SIM <Smile className="ml-2" />
             </button>
@@ -80,15 +83,19 @@ export function Home() {
               id="noButtonId"
               onClick={handleRandomMoveNoButton}
               onMouseEnter={handleRandomMoveNoButton}
-              className="flex flex-row items-center justify-center bg-main-red w-40 h-10 rounded-lg transition-all text-2xl font-bold text-slate-100 shadow-lg hover:bg-red-600 hover:shadow-xl"
+              className="flex z-20 flex-row items-center justify-center bg-main-red w-40 h-10 rounded-lg transition-all text-2xl font-bold text-slate-100 shadow-lg hover:bg-red-600 hover:shadow-xl max-sm:w-52"
             >
               NÃO <Frown className="ml-2" />
             </button>
           </div>
         </div>
 
-        <div className="w-2/4 h-full flex flex-col items-center justify-center  pr-10 max-sm:h-2/4 max-sm:pr-0 max-sm:w-full ">
-          <img src={questionSvg} alt="question svg" className="w-4/5 ml-10 " />
+        <div className="w-2/4 h-full flex flex-col items-center justify-center  pr-10 max-sm:h-1/4 max-sm:pr-0 max-sm:w-full">
+          <img
+            src={questionSvg}
+            alt="question svg"
+            className="w-4/5 ml-10 max-sm:w-2/5"
+          />
         </div>
       </div>
     </div>
