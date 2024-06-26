a840810465471050fbfefe3b7138b04dce4a14e6
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-06-26 15:55:22 -0300

    Implements balloons

---
+++ src/components/balloons.tsx
@@ -0,0 +1,61 @@
+export function Balloons() {
+  function random(num: number) {
+    return Math.floor(Math.random() * num);
+  }
+
+  function getRandomStyles() {
+    const r = random(255);
+    const g = random(255);
+    const b = random(255);
+    const mt = random(200);
+    const ml = random(50);
+    const dur = random(5) + 5;
+
+    return `
+    background-color: rgba(${r},${g},${b},0.7);
+    color: rgba(${r},${g},${b},0.7); 
+    box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
+    margin: ${mt}px 0 0 ${ml}px;
+    animation: float ${dur}s ease-in infinite
+    `;
+  }
+
+  function createBalloons(num) {
+    for (let i = num; i > 0; i--) {
+      const balloonContainer = document.getElementById("balloon-container")!;
+
+      const balloon = document.createElement("div");
+
+      balloon.className = "balloon";
+      balloon.style.cssText = getRandomStyles();
+      balloonContainer.append(balloon);
+    }
+  }
+
+  // function removeBalloons() {
+  //   const balloonContainer = document.getElementById("balloon-container")!;
+
+  //   console.log("ID: ", balloonContainer);
+
+  //   // balloonContainer.style.opacity = 0;
+  //   balloonContainer.style.opacity = "0";
+
+  //   setTimeout(() => {
+  //     balloonContainer.remove();
+  //   }, 500);
+  // }
+
+  window.addEventListener("load", () => {
+    createBalloons(30);
+  });
+
+  // window.addEventListener("click", () => {
+  //   removeBalloons();
+  // });
+
+  return (
+    <>
+      <div id="balloon-container" />
+    </>
+  );
+}
--- src/routes/accepted.tsx
+++ src/routes/accepted.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import { PartyPopper } from "lucide-react";
 
 import { Waves } from "../components/waves";
+import { Balloons } from "../components/balloons";
 
 import dogGif from "../assets/dogGif.gif";
 import romanticMusic from "../assets/romanticMusic.mp3";
@@ -15,36 +16,46 @@ export function Accepted() {
   }, []);
 
   return (
-    <div className="overflow-hidden">
-      <Waves />
+    <>
+      <div className="overflow-hidden">
+        <Waves />
+        <Balloons />
 
-      {/* <audio ref={audioRef}>
+        {/* <audio ref={audioRef}>
         <source src={romanticMusic} type="audio/mp3" />
       </audio> */}
 
-      <div className="bg-slate-100 w-screen h-screen flex items-center justify-center flex-col">
-        <div className="size-64 z-10 shadow-2xl rounded-full overflow-hidden flex items-center justify-center">
-          <img src={dogGif} className="w-full" />
-        </div>
+        <div className="bg-slate-100 w-screen h-screen flex items-center justify-center flex-col">
+          <div className="size-64 z-10 shadow-2xl rounded-full overflow-hidden flex items-center justify-center">
+            <img src={dogGif} className="w-full" />
+          </div>
 
-        <h1 className="text-main-text text-3xl font-semibold mt-20 flex flex-row items-center justify-center gap-4 z-10 text-center max-sm:w-11/12">
-          <PartyPopper color="#F2575F" size={50} />
-          😉 Sabia que iria escolher SIM 😏
-          <PartyPopper color="#F2575F" size={50} />
-        </h1>
-        {/* Então agora estamos oficialmente saindo 💐. Se arrume bem linda, (não
-        que seja possível você ficar mais linda ainda 😍), que mais tarde eu
+          <div className="flex flex-row items-center justify-center mt-20 z-10 max-sm:w-11/12">
+            <PartyPopper color="#F2575F" size={50} />
+            <h1 className="text-3xl ml-1 mr-4">😉</h1>
+
+            <h1 className="text-main-text text-3xl font-semibold text-center">
+              Sabia que iria escolher SIM
+            </h1>
+
+            <h1 className="text-3xl  ml-4 mr-1">😏</h1>
+            <PartyPopper color="#F2575F" size={50} />
+          </div>
+
+          {/* Então agora estamos oficialmente saindo 💐. Se arrume bem linda, (não
+        que seja possível você ficar mais linda ainda 😍), pois mais tarde eu
         estou passando ai pra te buscar 😎. */}
-        <p className="text-slate-700 text-base font-thin mt-6 w-3/4 z-10 max-sm:text-center max-sm:w-11/12">
-          Non ad veniam tempor anim fugiat eu sunt fugiat nulla amet in culpa.
-          Proident nisi ex deserunt irure ut laboris id aliqua excepteur in
-          eiusmod nisi sunt. Ipsum labore nisi culpa aliquip elit enim sint
-          commodo. Tempor et amet irure ipsum sint ad quis exercitation minim.
-          Est nisi aliqua ullamco ipsum fugiat est aliquip aliquip quis. In ea
-          aliquip id pariatur nisi reprehenderit consequat laborum laborum
-          commodo aliquip sint. Est minim magna cupidatat et.
-        </p>
+          <p className="text-slate-700 text-base font-thin mt-6 w-3/4 z-10 max-sm:text-center max-sm:w-11/12">
+            Non ad veniam tempor anim fugiat eu sunt fugiat nulla amet in culpa.
+            Proident nisi ex deserunt irure ut laboris id aliqua excepteur in
+            eiusmod nisi sunt. Ipsum labore nisi culpa aliquip elit enim sint
+            commodo. Tempor et amet irure ipsum sint ad quis exercitation minim.
+            Est nisi aliqua ullamco ipsum fugiat est aliquip aliquip quis. In ea
+            aliquip id pariatur nisi reprehenderit consequat laborum laborum
+            commodo aliquip sint. Est minim magna cupidatat et.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
